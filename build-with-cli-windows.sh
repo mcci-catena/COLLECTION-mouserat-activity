ab34ad4eb4bfd53b39fc66a3fec581edc7427b64
unknown <sjpark@mcci.com>
2021-02-03 15:42:27 -0500

    add scripts

---
+++ build-with-cli-windows.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# download arduino-cli from the following link:
+# https://downloads.arduino.cc/arduino-cli/arduino-cli_latest_Windows_64bit.zip
+
+# Once downloaded, extract the binary arduino-cli into a directory that is in your PATH.
+
+# exit if any errors encountered
+set -e
+
+export OUTPUT=C:\\tmp\\build-Catena4430-Sensor
+
+# make sure everything is clean
+if [[ "$1" = "--clean" ]]; then
+    rm -rf "$OUTPUT"
+fi
+
+# do a build
+arduino-cli compile \
+    -b mcci:stm32:mcci_catena_4610 \
+    --build-path "$OUTPUT" \
+    --build-property \
+xserial=generic,\
+sysclk=msi2097k,\
+opt=osstd,\
+lorawan_region=us915,\
+lorawan_network=ttn,\
+lorawan_subband=default \
+    --libraries libraries \
+    libraries/mcci-catena-4430/examples/Catena4430_Sensor/Catena4430_Sensor.ino
