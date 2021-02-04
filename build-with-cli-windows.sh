02b6df0b376e986b7c8a9a2a0c2670d463429c4d
unknown <sjpark@mcci.com>
2021-02-04 13:35:26 -0500

    remove properties and add them to fqbn

--- build-with-cli-windows.sh
+++ build-with-cli-windows.sh
@@ -17,14 +17,14 @@ fi
 
 # do a build
 arduino-cli compile \
-    -b mcci:stm32:mcci_catena_4610 \
-    --build-path "$OUTPUT" \
-    --build-property \
+    -b mcci:stm32:mcci_catena_4610:\
+upload_method=DFU,\
 xserial=generic,\
 sysclk=msi2097k,\
 opt=osstd,\
 lorawan_region=us915,\
 lorawan_network=ttn,\
 lorawan_subband=default \
+    --build-path "$OUTPUT" \
     --libraries libraries \
     libraries/mcci-catena-4430/examples/Catena4430_Sensor/Catena4430_Sensor.ino
